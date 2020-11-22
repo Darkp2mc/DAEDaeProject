ec400f6f1da628ac3b89d061351cdb667f1c5ca4
Hugo Lopes Domingues <2181043@my.ipleiria.pt>
2020-11-22 17:03:21 +0000

    Criaçao das classes mail, ja envia mails para o fake mail-otimizaçao

--- src/main/java/ejbs/EmailBean.java
+++ src/main/java/ejbs/EmailBean.java
@@ -9,28 +9,28 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Stateless(name = "EmailEJB")
 public class EmailBean {
     @Resource(name = "java:/jboss/mail/fakeSMTP")
     private Session mailSession;
-    public void send(String to, String subject, String text) throws MessagingException {
-        Message message = new MimeMessage(mailSession);
-        try {
-// Adjust the recipients. Here we have only one recipient.
-// The recipient's address must be an object of the InternetAddress class.
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
-            // Set the message's subject
-            message.setSubject(subject);
-// Insert the message's body
-            message.setText(text);
-// Adjust the date of sending the message
-            Date timeStamp = new Date();
-            message.setSentDate(timeStamp);
-// Use the 'send' static method of the Transport class to send the message
-            Transport.send(message);
-        } catch (MessagingException e) {
-            throw e;
-        }
+    private static final Logger logger = Logger.getLogger("EmailBean.logger");
+    public void send(String to, String subject, String text) {
+        Thread emailJob = new Thread(() -> {
+            Message message = new MimeMessage(mailSession);
+            Date timestamp = new Date();
+            try {
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
+                message.setSubject(subject);
+                message.setText(text);
+                message.setSentDate(timestamp);
+                Transport.send(message);
+            } catch (MessagingException e) {
+                logger.log(Level.SEVERE, e.getMessage());
+            }
+        });
+        emailJob.start();
     }
 }
