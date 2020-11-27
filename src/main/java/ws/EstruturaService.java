91358a08c0ec22e927d4a34cee9e6abcc85f866a
Pedro Miguel Carvalho Marrazes <2181186@my.ipleiria.pt>
2020-11-27 22:52:22 +0000

    Fixed EstruturaService

--- src/main/java/ws/EstruturaService.java
+++ src/main/java/ws/EstruturaService.java
@@ -16,7 +16,7 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Path("/projetistas")
+@Path("/estruturas")
 @Produces({MediaType.APPLICATION_JSON})
 @Consumes({MediaType.APPLICATION_JSON})
 
